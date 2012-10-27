d8d7b4d045c6a380abb179084e33d95ffaefef7d
J Clark <jeremy_clark1@yahoo.com>
2012-10-27 08:28:52 -0400

    Added support to delete a subscription. Issue#7.

--- p2.uppadada.com/controllers/c_subscriptions.php
+++ p2.uppadada.com/controllers/c_subscriptions.php
@@ -38,23 +38,22 @@ class subscriptions_controller extends base_controller {
 	
 	
 	/**
- 	* Check whether a subscription exists
+ 	* Delete a subscription
 	* 
-	* Check whether I'm subscribed to a user
 	*
-	* @subscribed_id The id of the user to follow
+	* @subscribed_id The id of the user to that I want to "unsubscribe" from
 	* 
 	*/
 	public function destroy($subscribed_id) {
 	
 		Auth::bounce(!$this->user);
 		
-		$data = Array("created" => Time::now(), "subscriber_id" => $this->user->user_id, "subscribed_id" => $subscribed_id);
-		
-		#Create a record in the "subscription" that joins you to the person you want to follow
-		DB::instance(DB_NAME)->insert("subscriptions", $data);
-		
-		Router::redirect("/posts/index");
+		# Delete this connection
+		$where_condition = 'WHERE subscriber_id = '.$this->user->user_id.' AND subscribed_id = '.$subscribed_id;
+		DB::instance(DB_NAME)->delete('subscriptions', $where_condition);
+
+		# Send them back
+		Router::redirect("/users/search");
 		
 	}
 	
