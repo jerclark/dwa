1ad3df51d831ff236a85e0461eb8931945404e29
J Clark <jeremy_clark1@yahoo.com>
2012-11-02 09:33:47 -0400

    Display a link to the users index when there are no posts available

--- p2.uppadada.com/views/v_posts_search_results.php
+++ p2.uppadada.com/views/v_posts_search_results.php
@@ -1,20 +1,30 @@
 <h2>Recent Activity</h2>
-<? foreach($results as $next_post): ?>
-<div class="content_box">
-	<div class="post_wrapper">
-		
-		<div style="display:table-cell;vertical-align:middle;">
-			<div class="profile_image" style="float:left;background-image:url('/profile_images/<?=$next_post['user_id']?>_profile.png');"></div>
-		</div>
-		
-		<div style="display:table-cell;vertical-align:middle;">
-			<span class="post_display_name" style="display"><?=$next_post['first_name']." ".$next_post['last_name']?></span>
-			&nbsp;
-			<span class="post_timestamp">planted this seed on: <?=$next_post['modified']?> GMT</span>
-		</div>
+<? if(count($results)==0): ?>
+
+	<div class="content_box">
+		<h2>There is no recent activity - <a href="/users/search">Click here to follow some friends!</a></h2>
 	</div>
+
+<? else: ?>
+
+	<? foreach($results as $next_post): ?>
+	<div class="content_box">
+		<div class="post_wrapper">
+		
+			<div style="display:table-cell;vertical-align:middle;">
+				<div class="profile_image" style="float:left;background-image:url('/profile_images/<?=$next_post['user_id']?>_profile.png');"></div>
+			</div>
+		
+			<div style="display:table-cell;vertical-align:middle;">
+				<span class="post_display_name" style="display"><?=$next_post['first_name']." ".$next_post['last_name']?></span>
+				&nbsp;
+				<span class="post_timestamp">planted this seed on: <?=$next_post['modified']?> GMT</span>
+			</div>
+		</div>
 	
-	<div class="post_text" style="clear:left;"><?=nl2br($next_post['text'])?></div>
+		<div class="post_text" style="clear:left;"><?=nl2br($next_post['text'])?></div>
 	
-</div>
-<? endforeach; ?>
+	</div>
+	<? endforeach; ?>
+	
+<? endif; ?>
