166477f6ecb936ec6a87afc36d45d6e932f3d248
J Clark <jeremy_clark1@yahoo.com>
2012-11-01 22:31:35 -0400

    Added some more info to the splash page

--- p2.uppadada.com/views/v_index_index.php
+++ p2.uppadada.com/views/v_index_index.php
@@ -2,6 +2,18 @@
 	
 	<br>
 	<h2>Welcome to the Chestnut Tree Cafe - Plant your seed of truth!</h2>
+	The Chestnut Tree Cafe is a simple microblog. You can:
+	<ul>
+	<li>Sign up for an account</li>
+	<li>View a list of other users</li>
+	<li>Follow other user's posts</li>
+	<li>Unfollow somebody you're following</li>
+	<li>View a list of all posts of people you're following</li>
+	<h3>You can also:</h3>
+	<li>Search for users to follow by name or email</li>
+	<li>Search for posts based on name or e-mail of poster, or content of the post</li>
+	<li>Add a profile picture</li>
+	</ul>
 	<a href="/users/login"><h3>Click Here to login</h3></a>
 	<h4>First time here?&nbsp;<a href="/users/signup_edit">Click here to join!</a></h4>
 
